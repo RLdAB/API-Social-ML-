26bc542cd98811a42c3b61f1fe2e46cee34a224e
RLdAB <robertoluizab@gmail.com>
2026-01-20 15:53:27 -0300

    refactor: reorganize project structure and implement user services

--- cmd/server/routes.go
+++ cmd/server/routes.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"github.com/RLdAB/API-Social-ML/internal/user/infrastructure/api"
+	postapi "github.com/RLdAB/API-Social-ML/internal/post/infrastructure/api"
+	userapi "github.com/RLdAB/API-Social-ML/internal/user/infrastructure/api"
 	"github.com/go-chi/chi/v5"
 )
 
-func setupRoutes(r *chi.Mux, userHandlers *api.UserHandlers) {
-	//Rotas do User
-	r.Post("/users", userHandlers.CreateUser)                                           // US - 0001
+func setupRoutes(r *chi.Mux, userHandlers *userapi.UserHandlers, postHandlers *postapi.PostHandlers) {
+	// Rotas do User
+	r.Post("/users", userHandlers.CreateUser)                                           // US-0001
 	r.Post("/users/{userId}/follow/{sellerId}", userHandlers.FollowUser)                // US-0001
 	r.Get("/users/{userId}/followers/list", userHandlers.GetFollowerList)               // US-0003
 	r.Get("/users/{userId}/followers/count", userHandlers.GetFollowersCount)            // US-0002
@@ -18,10 +19,13 @@ func setupRoutes(r *chi.Mux, userHandlers *api.UserHandlers) {
 	r.Get("/users/{userId}/followers/list", userHandlers.GetFollowerList)               // US-0008
 	r.Get("/users/{userId}/followed/list", userHandlers.GetFollowingList)               // US-0008
 	r.Get("/products/followed/{userId}/list", userHandlers.GetRecentFollowedPosts)      // US-0009
-	r.Post("/products/promo-pub", userHandlers.CreatePromoProduct)                      // US-0010
 	r.Get("/sellers/{sellerId}/promotions/count", userHandlers.CountPromotionsBySeller) // US-0011
 	r.Get("/users", userHandlers.ListUsers)
 	r.Get("/users/{userId}", userHandlers.GetUserByID)
 	r.Put("/users/{userId}", userHandlers.UpdateUser)
-	//r.Get("/products/followed/latest/{userId}", postHandlers.GetRecentPosts) // US-0006
+
+	// Rotas de Produtos/Promoções
+	r.Route("/products", func(r chi.Router) {
+		r.Post("/promo-pub", postHandlers.CreatePromoPost) // US-0010
+	})
 }
